e9a7abf186321f7c758f37f0980111cbcba5a62a
NeelBollapragada <neelbollapragada@gmail.com>
2025-07-01 19:25:49 +0100

    added edit note functionality

--- app/notes/index.jsx
+++ app/notes/index.jsx
@@ -74,6 +74,25 @@ const NoteScreen = () => {
     ]);
   };
 
+  const editNote = async (id, newText) => {
+    if (!newText.trim()) {
+      Alert.alert("Error", "Note text cannot be empty");
+      return;
+    }
+
+    const response = await noteService.updateNote(id, newText);
+
+    if (response.error) {
+      Alert.alert("Error", response.error);
+    } else {
+      setNotes((prevNotes) =>
+        prevNotes.map((note) =>
+          note.$id === id ? { ...note, text: response.data.text } : note
+        )
+      );
+    }
+  };
+
   return (
     <View style={styles.container}>
       {loading ? (
@@ -81,7 +100,7 @@ const NoteScreen = () => {
       ) : (
         <>
           {error && <Text style={styles.errorText}>{error}</Text>}
-          <NoteList notes={notes} onDelete={deleteNote} />
+          <NoteList notes={notes} onDelete={deleteNote} onEdit={editNote} />
         </>
       )}
 
